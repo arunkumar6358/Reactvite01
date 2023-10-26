5f436b2d604c0c3a1fcab23cf6ed8372bb76a14c
ARUNKUMAR.K <58990344+arunkumar6358@users.noreply.github.com>
2023-10-26 12:29:38 -0400

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Button } from 'react-bootstrap'
+import "../styles/Home.css"
+export const Home = () => {
+  return (
+    <Container>
+        <h1>Welcome to our website</h1>
+        <p>We are here to serve you</p>
+        <Button variant="primary" type="submit">Get started</Button>
+    </Container>
+  )
+}
